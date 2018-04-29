7349dc14ce4c41916568c94463645986a27fb7af
agouda27 <36905489+agouda27@users.noreply.github.com>
2018-04-29 18:39:27 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,22 +1,17 @@
+
+
 $('.identifier-col').on('click', function(e) {
-	let input = "";
-  if (e.target !== this){
-	if(e.target.name === "Identifier"){
-		alert('Identifier')
-		input = e.target.value;
-	}
-	if(e.target.value === "Query"){
-		alert('Query')
-		console.log(input);
-	}
-		
-}
-	
-  else{
-  alert( 'clicked the col' );
-  console.log(this)
-  	};
+	const $targ = $(e.target);
+	if ($targ.is('[name="Pin1"]')) {
+		console.log($targ.parent().find('[name="Identifier"]').val())
+	}	
 
 });
 	
 
+$('[name="Identifier"]').on('keydown', e => {
+	if (e.keyCode === 13) {
+		e.preventDefault();//Add Validator Function
+		console.log($(e.target).val())
+	}
+})
